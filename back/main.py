aa2b237ed30bbc49f6c214d441697c7a33687098
mac67 <macbookairm2@Mac.attlocal.net>
2025-06-27 17:18:37 -0400

    database

--- back/main.py
+++ back/main.py
@@ -1,27 +1,122 @@
 import uvicorn
 import re
-from fastapi import FastAPI, HTTPException
+import models
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from googleReview import fetch_reviews
-from typing import List, Optional
+from typing import List, Optional, Annotated
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from datetime import date
+from collections import defaultdict
 
 app = FastAPI()
 
+models.Base.metadata.drop_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
+
+# class NameBase(BaseModel):
+#     name_text:str
+
+# class ReviewBase(BaseModel):
+#     review_text:str
+#     names: List[NameBase]
+
+# @app.get("/names/{name_text}")
+# async def read_name(name_text: str, db: db_dependency):
+#     result = db.query(models.Names).filter(models.Names.name_text == name_text).all()
+#     if not result:
+#         raise HTTPException(status_code=404, detail='name not found')
+#     return result
+
+# @app.post("/reviews/")
+# async def create_reviews(reviews: ReviewBase, db: db_dependency):
+#     db_review = models.Reviews(review_text=reviews.review_text)
+#     db.add(db_review)
+#     db.commit()
+#     db.refresh(db_review)
+#     for name in reviews.names:
+#         db_name = models.Names(name_text=name.name_text, review_id=db_review.id)
+#         db.add(db_name)
+#     db.commit()
+
+# @app.post("/api/daily_count/")
+# def appendCount(employee_name: str, count_value: int, db: db_dependency):
+    
+#     today = date.today()
+
+#     emp = db.query(models.Employees).filter(models.Employees.employee_name == employee_name).first()
+#     if not emp:
+#         emp = models.Employees(employee_name=employee_name)
+#         db.add(emp)
+#         db.commit()
+#         db.refresh(emp)
+
+#     cnt = db.query(models.Counts).filter(models.Counts.employee_id == emp.id).first()
+#     if not cnt:
+#         cnt = models.Counts(employee_id=emp.id, date=today, count=count_value)
+#         db.add(cnt)
+#     else:
+#         cnt.count = count_value
+    
+#     db.commit()
+#     db.refresh(cnt)
+
+#     return {
+#         "employee": emp.employee_name,
+#         "date": cnt.date.isoformat(),
+#         "count": cnt.count
+#     }
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+NAMES = [
+    "AJ",
+    "Angelina",
+    "Averey",
+    "Avery",
+    "Cathy",
+    "Christine",
+    "Doan",
+    "Dorothy",
+    "Ella",
+    "Emma",
+    "Isaack",
+    "Kaylee",
+    "Lucas",
+    "Mathew",
+    "Matthew",
+    "Mina",
+    "Quincy",
+    "Reuben",
+    "Sissy",
+    "Victoria",
+    "Yixin"
+]
+
 class ReviewsRequest(BaseModel):
-    place_url: str
+    place_url: str = "https://www.google.com/maps/place/Mirai+Arcade/@28.5537855,-81.204756,17z/data=!4m8!3m7!1s0x88e767bc998e5b93:0x9c7c2a15715f540c!8m2!3d28.5537855!4d-81.2021811!9m1!1b1!16s%2Fg%2F11w23m4mqy?hl=en-us&entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D"
     max_reviews: int = 10
-    specificDate: Optional[str] = None
-    specificStar: int = None
-    names: Optional[List[str]] = None
-    untilDate: Optional[str] = None
+    specificDate: Optional[str] = date.today()
+    specificStar: int = 5
+    names: Optional[List[str]] = NAMES
+    # untilDate: Optional[str] = None
 
 class ReviewOut(BaseModel):
-    reviewText: Optional[str]
-    stars: int
+    name: str
+    # reviewText: Optional[str]
+    # stars: int
     date: Optional[str]
 
 @app.post("/api/reviews", response_model=list[ReviewOut])
-async def get_reviews(req: ReviewsRequest):
+async def get_reviews(req: ReviewsRequest, db: db_dependency):
     try:
         raw = fetch_reviews(req.place_url, req.max_reviews)
     except Exception as e:
@@ -48,13 +143,38 @@ async def get_reviews(req: ReviewsRequest):
             rv for rv in filtered
             if any(pat.search(rv.get("text", "") or "") for pat in patterns.values())
         ]
+        matched = []
+        counts_today: dict[str, int] = defaultdict(int)
+        for rv in filtered:
+            text = (rv.get("text", "") or "")
+            for name, pat in patterns.items():
+                if pat.search(text):
+                    matched.append((rv, name))
+                    counts_today[name] += 1
+                    break
+        for name, cnt_value in counts_today.items():
+            emp = db.query(models.Employees).filter(models.Employees.employee_name == name).first()
+            if not emp:
+                emp = models.Employees(employee_name=name)
+                db.add(emp)
+                db.commit()
+                db.refresh(emp)
+        
+            daily = db.query(models.Counts).filter(models.Counts.employee_id == emp.id, models.Counts.date == date.today()).first()
+            if not daily:
+                daily = models.Counts(employee_id=emp.id, date=date.today(), count=cnt_value)
+                db.add(daily)
+            else:
+                daily.count = cnt_value
+            db.commit()
     return [
         ReviewOut(
-            reviewText=rv.get("text"),
-            stars=rv.get("stars"),
+            name = name,
+            # reviewText=rv.get("text"),
+            # stars=rv.get("stars"),
             date=rv.get("publishedAtDate"),
         )
-        for rv in filtered
+        for rv, name in matched
     ]
 
 if __name__ == "__main__":
